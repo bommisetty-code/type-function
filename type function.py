87a4703983d4ee1bfbedae76ff12cdf0957fe553
Bommisetty Sreenivasulu <sbommisetty89@gmail.com>
2026-04-03 09:00:21 +0530

    Add files via upload

---
+++ type function.py
@@ -0,0 +1,46 @@
+# input reading from user
+
+name =input()
+print(type(name), name)
+
+# reading integers from user
+num=int(input())
+print(type(num), num)
+
+#reading float value from user
+val=float(input())
+print(type(val), val)
+
+x=int(input())
+y=int(input())
+c=complex(x,y)
+print(type(c),(c))
+
+n=bool(input())
+print(type(n),n)
+
+n=bool(int(input()))
+print(type(n),(n))
+
+#one egg price is 10 rupees and what is the price of 'n'eggs price#
+n=int(input())
+oneegg=10
+c=n*10
+print(c)
+
+#if box contains N pencils and total price is M,then price of each pencil price#
+n=int(input())
+m=int(input())
+c=m\n
+print(c)
+
+#if i buy N-red balls and M-green balls,than what is the total cost
+#red ball price is25 green ball price is30#
+n=int(input())
+m=int(input())
+a=n*25
+b=m*30
+print(a)
+print(b)
+
+          
